d4086bc340cb565f5d2dbbc80e015cda9daca97c
Helena Nakamura <helena.reiko@gmail.com>
2021-11-04 16:30:45 -0300

    SellerDaoJDBC and DaoFactory

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,7 @@ public class Program {
 		
 		Seller seller= new Seller(1, "Bob", "bob.gmail", new Date(), 3000.0, obj);
 		
+		SellerDao sellerDao=DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}

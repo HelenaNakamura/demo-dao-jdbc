46ea1cd830c5e79ae2b303aaff52a0ce82730abe
Helena Nakamura <helena.reiko@gmail.com>
2021-11-05 11:02:05 -0300

    insert implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -34,13 +34,21 @@ public class Program {
 		}
 		
 		System.out.println();
-		
 		System.out.println("***********teste 3 findAll*********");
 		
 		List<Seller>list1=sellerDao.findAll();
 		for (Seller obj1:list1) {
 			System.out.println(obj1);
 		}
+		
+		
+		System.out.println();
+		System.out.println("***********teste 4 insert*********");
+		
+		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new Id = "+ newSeller.getId());
+		
 	}
 
 }
